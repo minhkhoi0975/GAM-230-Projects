a3b6975e7a07a9617795ea4ad97f949f04c954f2
minhkhoi0975 <minhkhoi0975@gmail.com>
2021-04-09 02:33:38 -0400

    + Adjusted where the shell is fired from the turret.

--- Project 4 - Tank Battle/Assets/Script/NPCs/StationaryTankTurret.cs
+++ Project 4 - Tank Battle/Assets/Script/NPCs/StationaryTankTurret.cs
@@ -2,33 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerTankTurretController : MonoBehaviour
+public class StationaryTankTurret : MonoBehaviour
 {
     // Combat properties
     public int ammoCount = 3;
     public GameObject shell;
     public float fireRateInSeconds = 0.5f;
-    public float reloadRateInSeconds = 1.0f;
+    public float reloadRateInSeconds = 2.0f;
 
     bool readyToFire = true;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        // Get the position of the mouse on screen space.
-        Vector3 mousePosition = Input.mousePosition;
-
-        // Get the position of the turret on screen space.
-        Vector3 turretPosition = Camera.main.WorldToScreenPoint(transform.position);
-
-        // Rotate the turret to the position of the mouse.
-        mousePosition.x = mousePosition.x - turretPosition.x;
-        mousePosition.y = mousePosition.y - turretPosition.y;
-        float angle = Mathf.Atan2(mousePosition.x, mousePosition.y) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(new Vector3(0.0f, angle, 0.0f));
-
-        // Shoot.
-        if (Input.GetButton("Fire1") && readyToFire)
+        if (readyToFire)
         {
             StartCoroutine(Shoot());
         }
@@ -55,7 +41,6 @@ public class PlayerTankTurretController : MonoBehaviour
             readyToFire = true;
         }
 
-        // Out of ammo? Reload.
         if (ammoCount <= 0)
         {
             readyToFire = false;
