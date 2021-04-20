a94e94b5d00ee1f49e54955429f696326eeafa59
minhkhoi0975 <minhkhoi0975@gmail.com>
2021-04-20 11:43:59 -0400

    + Added minimap

---
+++ Project 4 - Tank Battle/Assets/Scripts/Camera/MinimapCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapCamera : MonoBehaviour
+{
+    public GameObject player;
+
+    Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = player.transform.position - gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+        {
+            // Always keep the same distance to the player.
+            gameObject.transform.position = player.transform.position - offset;
+        }
+    }
+}
